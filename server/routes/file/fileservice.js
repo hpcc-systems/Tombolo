8f4cc6087735ca16b06dd79a0049dc802f23c517
Jerry Jacob <jerry.jacob@lexisnexisrisk.com>
2019-08-14 15:55:20 -0400

    creating schemas dir if it does not exist while exporting ecl schema

--- server/routes/file/fileservice.js
+++ server/routes/file/fileservice.js
@@ -15,49 +15,58 @@ module.exports = {
 async function getECLSchema(appId, res) {
   var results = [], ds = '\tEXPORT DATASETS  := MODULE\n';
   var schema = 'EXPORT files := MODULE\n\n\t EXPORT layouts := MODULE\n\n';
-      try {
-          var app = await App.findOne({where:{"id":appId}});
-          var schemaFile = path.join(__dirname, '..', '..', 'schemas', app.title+'-schema.ecl');
-          var files = await File.findAll({where:{"application_id":appId}});
-          for(const file of files) {
-              var fileLayout = await FileLayout.findAll({where:{"application_id":appId, "file_id":file.id}});
-              var fileId = file.name.replace('-', '_');
-              schema += '\n\t\tEXPORT ' + fileId + ' := RECORD\n';
-              for(const field of fileLayout) {
-                schema += '\t\t\t' + field.type + ' ' + field.name + ';\n';
-              }
-              schema += '\t\tEND;\n\n'
 
-              ds+='\t\tEXPORT ' + fileId + ' := DATASET(\'~'+fileId+'\', layouts.'+fileId+',THOR);\n';
-              //schema += '\tEND;'
-
-          }
-
-          schema += '\n'+ ds + '\tEND;\n\nEND;';
-          console.log('schem-23: '+schema);
-
-          fs.appendFile(schemaFile, schema, function (err) {
-              if (err) throw err;
-              console.log('Saved! ');
-              res.download(schemaFile, function(err){
-                if (err) {
-                  console.log("Error occured during download...")
-                } else {
-                  console.log("Download completed...")
-                  fs.unlink(schemaFile, (err) => {
-                    if (err) throw err;
-                    console.log(schemaFile + ' was deleted after download');
-                  });
-                }
-              });
-          });
-      } catch (err) {
-          console.log('err', err);
+  try {
+      var schemaDir = path.join(__dirname, '..', '..', 'schemas');
+      if (!fs.existsSync(schemaDir)){
+        fs.mkdirSync(schemaDir);
       }
+      var app = await App.findOne({where:{"id":appId}});
+      var schemaFile = path.join(__dirname, '..', '..', 'schemas', app.title+'-schema.ecl');
+      var files = await File.findAll({where:{"application_id":appId}});
+      for(const file of files) {
+          var fileLayout = await FileLayout.findAll({where:{"application_id":appId, "file_id":file.id}});
+          var fileId = file.name.replace('-', '_');
+          schema += '\n\t\tEXPORT ' + fileId + ' := RECORD\n';
+          for(const field of fileLayout) {
+            schema += '\t\t\t' + field.type + ' ' + field.name + ';\n';
+          }
+          schema += '\t\tEND;\n\n'
+
+          ds+='\t\tEXPORT ' + fileId + ' := DATASET(\'~'+fileId+'\', layouts.'+fileId+',THOR);\n';
+          //schema += '\tEND;'
+
+      }
+
+      schema += '\n'+ ds + '\tEND;\n\nEND;';
+      console.log('schem-23: '+schema);
+
+      fs.appendFile(schemaFile, schema, function (err) {
+          if (err) throw err;
+          console.log('Saved! ');
+          res.download(schemaFile, function(err){
+            if (err) {
+              console.log("Error occured during download...")
+            } else {
+              console.log("Download completed...")
+              fs.unlink(schemaFile, (err) => {
+                if (err) throw err;
+                console.log(schemaFile + ' was deleted after download');
+              });
+            }
+          });
+      });
+  } catch (err) {
+      console.log('err', err);
+  }
 }
 
 async function getJSONSchema(appId, res) {
   try {
+        var schemaDir = path.join(__dirname, '..', '..', 'schemas');
+        if (!fs.existsSync(schemaDir)){
+          fs.mkdirSync(schemaDir);
+        }
         var schemaFile = path.join(__dirname, '..', '..', 'schemas', appId+'-schema.json');
         console.log('appId: '+appId);
         var result=[];
