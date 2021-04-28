bec06e7df117bf9b0d58e86521746d2608621634
Yadhap Dahal <yadhapdahal111@gmail.com>
2021-04-28 17:13:49 -0400

    Fixed UI issues

--- server/routes/user/userservice.js
+++ server/routes/user/userservice.js
@@ -7,7 +7,6 @@ let User = models.user;
 const Sequelize = require('sequelize');
 let UserApplication = models.user_application;
 const authServiceUtil = require('../../utils/auth-service-utils');
-const chalk = require("chalk")
 
 async function authenticate(req, res, { username, password }) {
     var authServiceUrl = process.env.AUTH_SERVICE_URL + '/login';
@@ -133,10 +132,6 @@ const searchUser = (req, res, next) => {
 async function getById(id) {
     return await User.findOne({where: {"id":id}, attributes: { exclude: ["hash"] }});
 }
-
-
-
-
 
 async function create(userParam) {
     // validate
@@ -297,10 +292,7 @@ async function registerUser(req, res) {
 }
 
 async function forgotPassword(req, res) {
-  console.log(chalk.green("<<<< forget PW function exectuted ", req.body.email ))
   var authServiceUrl = process.env.AUTH_SERVICE_URL + '/forgotPassword';
-  console.log(chalk.green("<<<< post URL", authServiceUrl ))
-
   return new Promise(function(resolve, reject) {
     request.post({
       url: authServiceUrl,
@@ -308,7 +300,7 @@ async function forgotPassword(req, res) {
         "content-type": "application/json"
       },
       json: {
-        "email": req.body.email,
+        "username": req.body.username,
         "clientId": process.env.AUTHSERVICE_TOMBOLO_CLIENT_ID,
         "resetUrl": process.env.TOMBOLO_PASSWORD_RESET_URL
       }
