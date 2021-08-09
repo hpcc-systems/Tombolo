d31d92d43753e83f134860c5376f9b0f6dd3e5bd
alfjjacob <jerry.jacob@lexisnexisrisk.com>
2021-08-09 15:27:35 -0400

    workflow execution changes
    removed kafka integration for triggering jobs
    fixed issues introduced from adding soft delete

--- server/jobs/submitJob.js
+++ server/jobs/submitJob.js
@@ -2,6 +2,8 @@ const { parentPort, workerData } = require("worker_threads");
 const request = require('request-promise');
 const hpccUtil = require('../utils/hpcc-util');
 const assetUtil = require('../utils/assets.js');
+const workflowUtil = require('../utils/workflow-util.js');
+const JobScheduler = require('../job-scheduler');
 
 let isCancelled = false;
 if (parentPort) {
@@ -11,8 +13,8 @@ if (parentPort) {
 }
 
 (async () => {
+  let wuResult, wuid='';
   try {
-    let wuid='';
     if(workerData.jobType == 'Spray') {
       let sprayJobExecution = await hpccUtil.executeSprayJob({
         cluster_id: workerData.clusterId, 
@@ -28,15 +30,18 @@ if (parentPort) {
     `submitting job ${workerData.jobName} ` +
     `(WU: ${wuid}) to url ${workerData.clusterId}/WsWorkunits/WUResubmit.json?ver_=1.78`
     );
-    let wuInfo = await hpccUtil.resubmitWU(workerData.clusterId, wuid);
+    let wuInfo = await hpccUtil.resubmitWU(workerData.clusterId, wuid);    
+    workerData.status = 'submitted';
     //record workflow execution
-    let jobExecutionRecorded = await assetUtil.recordJobExecution(workerData, wuid);
+    let jobExecutionRecorded = await assetUtil.recordJobExecution(workerData, wuid);          
+    
   } catch (err) {
     console.log(err);
   } finally {
-    if (parentPort) {
+    if (parentPort) {            
       console.log(`signaling done for ${workerData.jobName}`)
-      parentPort.postMessage('done');
+      parentPort.postMessage('done');      
+      
     } else {
       process.exit(0);
     }
