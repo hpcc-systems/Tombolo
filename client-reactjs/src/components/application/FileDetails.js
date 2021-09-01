91414fa9d43cb7d3edba25a50a45b5968ac3f2b0
alfjjacob <jerry.jacob@lexisnexisrisk.com>
2021-08-31 21:34:32 -0400

    added null check for consumer type

--- client-reactjs/src/components/application/FileDetails.js
+++ client-reactjs/src/components/application/FileDetails.js
@@ -1538,7 +1538,7 @@ class FileDetails extends PureComponent {
                       {!this.state.enableEdit ? (
                         this.props.consumers.map((consumer) => {
                           if (
-                            consumer.assetType.indexOf("Supplier") != -1 &&
+                            consumer.assetType && consumer.assetType.indexOf("Supplier") != -1 &&
                             consumer.id === this.state.file.supplier
                           ) {
                             return consumer.name;
@@ -1558,7 +1558,7 @@ class FileDetails extends PureComponent {
                           disabled={!editingAllowed}
                         >
                           {this.props.consumers.map((consumer) =>
-                            consumer.assetType.indexOf("Supplier") != -1 ? (
+                            consumer.assetType && consumer.assetType.indexOf("Supplier") != -1 ? (
                               <Option key={consumer.id}>{consumer.name}</Option>
                             ) : null
                           )}
@@ -1586,7 +1586,7 @@ class FileDetails extends PureComponent {
                           disabled={!editingAllowed}
                         >
                           {this.props.consumers.map((consumer) =>
-                            consumer.assetType.indexOf("Consumer") != -1 ? (
+                            consumer.assetType && consumer.assetType.indexOf("Consumer") != -1 ? (
                               <Option key={consumer.id}>{consumer.name}</Option>
                             ) : null
                           )}
@@ -1594,7 +1594,7 @@ class FileDetails extends PureComponent {
                       ) : (
                         this.props.consumers.map((consumer) => {
                           if (
-                            consumer.assetType.indexOf("Consumer") != -1 &&
+                            consumer.assetType && consumer.assetType.indexOf("Consumer") != -1 &&
                             consumer.id === this.state.file.consumer
                           ) {
                             return consumer.name;
@@ -1613,7 +1613,7 @@ class FileDetails extends PureComponent {
                       {!this.state.enableEdit ? (
                         this.props.consumers.map((consumer) => {
                           if (
-                            consumer.assetType.indexOf("Owner") != -1 &&
+                            consumer.assetType && consumer.assetType.indexOf("Owner") != -1 &&
                             consumer.id === this.state.file.owner
                           ) {
                             return consumer.name;
@@ -1633,7 +1633,7 @@ class FileDetails extends PureComponent {
                           disabled={!editingAllowed}
                         >
                           {this.props.consumers.map((consumer) =>
-                            consumer.assetType.indexOf("Owner") != -1 ? (
+                            consumer.assetType && consumer.assetType.indexOf("Owner") != -1 ? (
                               <Option key={consumer.id}>{consumer.name}</Option>
                             ) : null
                           )}
@@ -1809,6 +1809,7 @@ function mapStateToProps(state) {
   const { user } = state.authenticationReducer;
   console.log(state.applicationReducer)
   const { application, clusters, consumers } = state.applicationReducer;
+  console.log(consumers);
 
   const { isNew = false, groupId = "" } = newAsset;
   return {
