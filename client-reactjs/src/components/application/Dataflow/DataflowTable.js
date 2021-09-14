fe133c83606c654715ae96574bd2c40d0b5da855
yadhap Dahal <yadhap.dahal@lexisnexisrisk.com>
2021-09-14 09:24:08 -0400

    added null check

--- client-reactjs/src/components/application/Dataflow/DataflowTable.js
+++ client-reactjs/src/components/application/Dataflow/DataflowTable.js
@@ -74,6 +74,7 @@ function DataflowTable({data, applicationId, onSelectDataflow, onDataFlowUpdated
     ellipsis: true,
     width: '30%',
     render: (text, record) => { 
+      console.log("text <<<<<<<<<<", text)
       const converter = new showdown.Converter();
       if(text){
         return converter.makeHtml(text).replace(/<[^>]*>/g, "")
