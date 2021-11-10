8a0768b3839e37267b5f45494c0a586c71e6b0b5
yadhap Dahal <yadhap.dahal@lexisnexisrisk.com>
2021-11-09 23:22:56 -0500

    removed mixed up code from azure ad branch

--- server/seeders/20200204185532-rules.js
+++ server/seeders/20200204185532-rules.js
@@ -5,20 +5,20 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
       return queryInterface.bulkInsert('rules', [{
-        id       : uuidv4(),
-        name     : 'Date Validate',
-        createdAt: new Date(),
-        updatedAt: new Date()
+        id: uuidv4(),
+        name : 'Date Validate',
+        createdAt : new Date(),
+        updatedAt : new Date()
       },{
-        id       : uuidv4(),
-        name     : 'Phone Validate',
-        createdAt: new Date(),
-        updatedAt: new Date()
+        id: uuidv4(),
+        name : 'Phone Validate',
+        createdAt : new Date(),
+        updatedAt : new Date()
       },{
-        id       : uuidv4(),
-        name     : 'Integer Validate',
-        createdAt: new Date(),
-        updatedAt: new Date()
+        id: uuidv4(),
+        name : 'Integer Validate',
+        createdAt : new Date(),
+        updatedAt : new Date()
       },{
         id: uuidv4(),
         name : 'Not Empty',
