756f8f99cd51b97842d387f46db4a5c786f9e9da
yadhap Dahal <yadhap.dahal@lexisnexisrisk.com>
2021-11-17 11:49:16 -0500

    added cluster whitelist back

--- server/cluster-whitelist.js
+++ server/cluster-whitelist.js
@@ -7,26 +7,6 @@ module.exports = {
       roxie: "https://play.hpccsystems.com",
       roxie_port: "18002",
     },
-    {
-      name: "4-Way-2",
-      thor: "http://10.173.147.1",
-      thor_port: "8010",
-      roxie: "http://10.173.147.1",
-      roxie_port: "8002",
-    },
-    {
-      name : "new",
-      thor: "http://52.255.183.194",
-      thor_port : "8010",
-      roxie : "http://52.255.183.194",
-      roxie_port: "8002"
-    },
-    {
-      name: "4-Way",
-      thor: "https://discoverylab.hpcc.risk.regn.net",
-      thor_port: "18010",
-      roxie: "https://discoverylab.hpcc.risk.regn.net",
-      roxie_port: "18010",
-    },
+    
   ],
 };
