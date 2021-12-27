742d3bb1fd950e61fef13dd6f8fd15f5a1ccdf09
Kostiantyn Agapov <konstagapov123@gmail.com>
2021-12-27 08:26:41 -0800

    Refactor component

--- client-reactjs/src/components/application/AssetDetailsDialog.js
+++ client-reactjs/src/components/application/AssetDetailsDialog.js
@@ -1,85 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import { authHeader, handleError } from "../common/AuthHeader.js"
+import React, { useEffect } from 'react';
 import useFileDetailsForm from '../../hooks/useFileDetailsForm';
-import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { Modal, Button } from 'antd/lib';
-import _ from "lodash";
+import { useSelector } from 'react-redux';
+import { Modal } from 'antd/lib';
 
 function AssetDetailsDialog(props) {
-  const [application, setApplication] = useState({...props});
-
-	const { isShowing, toggle, OpenDetailsForm } = useFileDetailsForm();
-
-  let { assetType, applicationId, assetId, selectedDataflow } = useParams();
+  const { isShowing, toggle, OpenDetailsForm } = useFileDetailsForm();
+  const authReducer = useSelector((state) => state.authenticationReducer);
 
   useEffect(() => {
-  	setApplication({...props});
-    if (props.assetId != '') {
-      handleOpen();
-    }
-  }, [props.assetId])
-
-  const authReducer = useSelector(state => state.authenticationReducer);
-
-  const handleOpen = () => {
-    toggle();
-  }
+    if (props.assetId) toggle();
+  }, [props.assetId]);
 
   const handleClose = () => {
     toggle();
     //flip the state in the parent component
     props.handleClose();
-  }
+  };
 
-  const DetailsForm = props => {
-    return OpenDetailsForm({
-      ...props,
-      "type": props.assetType,
-      "isNew":false,
-      "selectedAsset": props.selectedAsset,
-      "selectedDataflow": props.selectedDataflow,
-      "nodes": props.nodes,
-      "edges": props.edges,
-      "nodeIndex": props.nodeIndex,
-      "application": props.application,
-      "user": props.user,
-      "onClose": handleClose,
-      "viewMode": true
-    })
-  }
+  const capitalize = (word) => {
+    if (!word || typeof word !== 'string') return '';
+    return word[0].toUpperCase() + word.slice(1);
+  };
 
-  return (
-	  (authReducer.user && authReducer.user.token != undefined) ?
-	  <React.Fragment>
-      
-	    <Modal
-          visible={isShowing}
-          width="1200px"
-          onCancel={handleClose}
-          footer={null}
-          bodyStyle={{display: "flex", flexDirection: "column"}}
-          title={_.startCase(_.toLower(props.assetType)) + " : " +   props.title}
-        > 
-        <DetailsForm
-          assetType={props.assetType}
-          assetId={props.assetId}
-          fileId={props.fileId}
-          application={props.application}
-          user={props.user}
-          selectedDataflow={props.selectedDataflow}
-          nodes={props.nodes}
-          edges={props.edges}
-          nodeIndex={props.nodeIndex}
-          displayingInModal={true}
-          reload={props.reload}
-          onAssetSaved={props.onAssetSaved}
-        />
+  const detailsFormProps = { ...props, isNew: false, onClose: handleClose, displayingInModal: true, type: props.assetType };
+
+  return authReducer.user && authReducer.user.token ? (
+    <React.Fragment>
+      <Modal
+        width='1200px'
+        footer={null}
+        visible={isShowing}
+        onCancel={handleClose}
+        bodyStyle={{ display: 'flex', flexDirection: 'column' }}
+        title={`${capitalize(props.assetType)} : ${props.title}`}
+      >
+        <OpenDetailsForm {...detailsFormProps} />
       </Modal>
-	  </React.Fragment>
-	  : null
-
-	)
+    </React.Fragment>
+  ) : null;
 }
 
-export default AssetDetailsDialog
+export default AssetDetailsDialog;
