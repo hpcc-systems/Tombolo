b0d61144aa6e3fde46c5df9952650c89de3ac7c4
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-01-19 13:03:17 -0800

    Refactore AssetDetailsDialog

--- client-reactjs/src/components/application/AssetDetailsDialog.js
+++ client-reactjs/src/components/application/AssetDetailsDialog.js
@@ -1,51 +1,40 @@
-import React, {  useEffect } from 'react'
-import useFileDetailsForm from '../../hooks/useFileDetailsForm';
-import { useSelector } from "react-redux";
+import React from 'react'
 import { Modal } from 'antd/lib';
 
+import FileDetailsForm from './FileDetails';
+import JobDetailsForm from './Jobs/JobDetails';
+import IndexDetailsForm from './IndexDetails';
+import QueryDetailsForm from './QueryDetails';
+
 function AssetDetailsDialog(props) {
-
-	const { isShowing, toggle, OpenDetailsForm } = useFileDetailsForm();
-  const authReducer = useSelector(state => state.authenticationReducer);
-
-  useEffect(() => {
-    console.log("asetdialogprops", props)
-    if (props.selectedAsset.id)  toggle(); // opens dialog
-  }, [props.selectedAsset.id])
-
-  const handleClose = () => {
-    toggle();
-    props.handleClose();
-  }
-
+  
   const capitalize = (word) => {
     if (!word || typeof word !== 'string') return '';
     return word[0].toUpperCase() + word.slice(1);
   };
 
+  const DetailsForm = (props) =>{
+    const formOptions ={
+      job: <JobDetailsForm {...props}/>,
+      file: <FileDetailsForm {...props} />,
+      index: <IndexDetailsForm {...props}/>,
+      query: <QueryDetailsForm  {...props}/>
+    }
+    const jobType = props.selectedJobType.toLowerCase();
+    return formOptions[jobType];
+  } 
+
   return(
-    <>
-      {authReducer.user?.token ?
-	    <Modal
-          visible={isShowing}
-          width="1200px"
-          onCancel={handleClose}
-          footer={null}
-          bodyStyle={{display: "flex", flexDirection: "column"}}
-          title={`${capitalize(props.selectedJobType)} : ${props.selectedNodeTitle}`}
-          > 
-        <OpenDetailsForm
-          {...props}
-          type={props.selectedJobType} // OpenDetailsForm needs this prop
-          viewMode={true} // ?
-          displayingInModal={true} // ?
-          fileId={props.selectedAsset.id} // ?
-          onClose={handleClose} 
-        />
-      </Modal>
-	
-	  : null}
-    </>
+    <Modal
+      visible={props.show}
+      onCancel={()=>props.onClose()}
+      width="1200px"
+      footer={null}
+      bodyStyle={{display: "flex", flexDirection: "column"}}
+      title={`${capitalize(props.selectedJobType)} : ${props.selectedNodeTitle}`}
+      > 
+      <DetailsForm {...props} />
+    </Modal>
 	)
 }
 
