36e7721c8f415aecc5177fc3890daaa369d6d894
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-01-28 17:07:47 -0800

    Custom button to delete node, needed to show confirm dialog first

--- client-reactjs/src/components/application/Graph/Event.js
+++ client-reactjs/src/components/application/Graph/Event.js
@@ -1,4 +1,7 @@
-import { debounce } from "lodash";
+import Modal from 'antd/lib/modal/Modal';
+import eyeSvg from './Icons/eye-invisible.svg'
+
+import { ExclamationCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 
 export default class Event {
   static init(graph, graphRef) {
@@ -7,35 +10,95 @@ export default class Event {
       const ports = graphRef.current.querySelectorAll('.x6-port-body');
       ports.forEach(port => (port.style.visibility = 'visible'));
       // add a remove button
-      node.addTools([
-        {
-          name: 'button-remove',
-   
-        },
+      // const tools = [{ name: 'button-remove', }];
+
+      const tools = [
         {
           name: 'button',
           args: {
-            x: '100%',
-            y: 3,
             markup: [
               {
+                tagName: 'circle',
+                selector: 'button',
+                attrs:{
+                  r: 6,
+                  stroke: '#ff0000',
+                  fill: '#ff0000',
+                  cursor: 'pointer',
+                }
+              },
+              {
                 tagName: 'text',
-                textContent: 'Hide',
+                textContent: 'X',
                 selector: 'icon',
                 attrs: {
-                  fill: '#fe854f',
-                  textAnchor: 'middle',
+                  fill: '#ffffff',
+                  y:3,
+                  "font-weight": 700,
+                  'font-size': 9,
+                  'text-anchor': 'middle',
+                  'pointer-events': 'none',
+                },
+              }
+        
+            ],
+            x: 0,
+            y: 0,
+            offset: { x: 5, y: 4},
+            onClick(param) { 
+               const cell = param.cell;
+               const nodeData = cell.getData();
+               Modal.confirm({
+                icon: <ExclamationCircleOutlined style={{color: "red"}} />,
+                title: `Do you really want to remove ${nodeData.title}?`,
+                okText: 'Yes',
+                cancelText: 'No',
+                okButtonProps: {type: 'danger'},
+                cancelButtonProps: {type: 'primary'},
+                onOk(){
+                  cell.remove();
+                },
+                onCancel() {
+                  return;
+                }
+              });
+             },
+          },
+        },
+      ];
+
+
+      if (node.data.type !== 'Job'){
+        const hideButton =  {
+          name: 'button',
+          args: {
+            x: "100%",
+            y: 0,
+            offset: { x: -13, y: -3},
+            markup: [
+              {
+                tagName: 'image',
+                selector: 'icon',
+                attrs: {
+                  width: 14,
+                  height: 14,
                   cursor: 'pointer',
-                  fontSize: '13px',
+                  'xlink:href': eyeSvg,
                 },
               },
             ],
             onClick({ cell }) {
-              console.log('hello');
+             console.log('-cell-----------------------------------------');
+             console.dir({cell}, { depth: null });
+             console.log('------------------------------------------');
+             
             },
           },
-        },
-      ]);
+        };
+        tools.push(hideButton);
+      }
+      
+      node.addTools(tools);
     });
 
     graph.on('node:mouseleave', ({ node }) => {
