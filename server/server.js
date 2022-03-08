79e26082138d1b466e5217be4bb634e71440435a
alfjjacob <jerry.jacob@lexisnexisrisk.com>
2022-03-08 17:00:24 -0500

    enabled cors for all - test

--- server/server.js
+++ server/server.js
@@ -30,8 +30,8 @@ const limiter = rateLimit({
 app.use(cors());
 app.use(express.json());
 app.use(function(req, res, next) {
-  //res.header("Access-Control-Allow-Origin", "*");
-  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 //apply to all requests
