b7b412db5ad035e7103a14211ab8cb18c2a96872
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-03-21 11:11:46 -0700

    adding strict comparison into permissions check

--- client-reactjs/src/components/common/AuthUtil.js
+++ client-reactjs/src/components/common/AuthUtil.js
@@ -1,11 +1,11 @@
 import { Constants } from './Constants';
 
 export function hasAdminRole(user) {
-	return (user.role && user.role.filter(role => role.name == Constants.TOMBOLO_ADMIN).length > 0)
+	return (user.role && user.role.filter(role => role.name === Constants.TOMBOLO_ADMIN).length > 0)
 }
 
 export function hasEditPermission(user) {
-	return (user.role && user.role.filter(role => (role.name == Constants.TOMBOLO_ADMIN || role.name == Constants.TOMBOLO_CREATOR)).length > 0)
+	return (user.role && user.role.filter(role => (role.name === Constants.TOMBOLO_ADMIN || role.name === Constants.TOMBOLO_CREATOR)).length > 0)
 }
 
 export function canViewPII(user) {
