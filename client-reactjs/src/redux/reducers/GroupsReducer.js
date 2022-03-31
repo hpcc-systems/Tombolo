972661260a48f41ecae8b1c94c71fc2712522f7a
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-03-30 17:33:48 -0700

    groupsReducer include global tree view, datalist, and loading state with error

--- client-reactjs/src/redux/reducers/GroupsReducer.js
+++ client-reactjs/src/redux/reducers/GroupsReducer.js
@@ -1,18 +1,27 @@
 import { Constants } from '../../components/common/Constants';
 
 const initialState = {
+  error:"",
+  loading: false,
+  tree : [],
+  dataList:[],
   expandedKeys:['0-0'],
   selectedKeys:{id:"", key:"0-0"}
 };
 
 export function groupsReducer(state = initialState, action) {
   switch (action.type) {
-    case Constants.GROUPS_EXPANDED:
-      return {
-        ...state,
-        expandedKeys: action.keys.expandedKeys,
-        selectedKeys: {id: action.keys.selectedKeys.id, key:action.keys.selectedKeys.key}
-      };
+      case Constants.GROUPS_EXPANDED:
+        return { ...state, expandedKeys: action.payload};
+      case Constants.SELECT_GROUP:
+        return {...state,  selectedKeys: action.payload }
+      case Constants.FETCH_GROUPS_TREE:
+        return { ...state, loading: true, error: ''};
+      case Constants.FETCH_GROUPS_TREE_SUCCESS:
+          return { ...state, loading: false, tree : action.payload.tree, dataList: action.payload.dataList };
+      case Constants.FETCH_GROUPS_TREE_ERROR:
+        return { ...state, loading: false, error: action.payload };
+
     default:
       return state
   }
