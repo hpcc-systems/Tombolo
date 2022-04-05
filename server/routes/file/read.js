1597510376e4d8f17c97d675a36fc23e5d0e9d47
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-04-05 14:57:47 -0700

    remove file or rename file while saving if it was associated to already existing file to avoid duplications

--- server/routes/file/read.js
+++ server/routes/file/read.js
@@ -24,6 +24,8 @@ const Op = Sequelize.Op;
 let Indexes=models.indexes;
 let Query=models.query;
 let Job=models.job;
+const JobFile = models.jobfile;
+let AssetDataflow = models.assets_dataflows;
 let Visualization=models.visualizations;
 var request = require('request');
 var requestPromise = require('request-promise');
@@ -413,7 +415,7 @@ router.post('/saveFile', [
       .isUUID(4).withMessage('Invalid application id'),
     body('file.basic.title')
     .matches(/^[a-zA-Z]{1}[a-zA-Z0-9_:.\-]*$/).withMessage('Invalid title')
-  ], (req, res) => {
+  ], async (req, res) => {
     console.log("[file list/read.js] - Get file list for app_id = " + req.body.file.app_id + " isNewFile: "+req.body.isNew);
     var fileId='', applicationId=req.body.file.app_id, fieldsToUpdate={};
 
@@ -422,6 +424,23 @@ router.post('/saveFile', [
       if (!errors.isEmpty()) {
         return res.status(422).json({ success: false, errors: errors.array() });
       }
+
+      const { removeAssetId = '', renameAssetId = '',  basic } = req.body.file;
+      const { name, cluster_id, application_id } = basic;
+      
+      // We want to delete design file if it was associated with existing file that is already in Tombolo DB
+      if (removeAssetId) {
+        await Promise.all([
+          JobFile.destroy({ where: { file_id: removeAssetId } }),
+          FileLayout.destroy({ where: { file_id: removeAssetId } }),
+          FileLicense.destroy({ where: { file_id: removeAssetId } }),
+          AssetDataflow.destroy({ where: { assetId: removeAssetId } }),
+          FileValidation.destroy({ where: { file_id: removeAssetId } }),
+          File.destroy({ where: { id: removeAssetId, application_id } }),
+        ]);
+      }
+      // We want to update design file if it was associated with HPCC file that was not in Tombolo DB
+      if (renameAssetId) await File.update({ name, cluster_id }, { where: { id: renameAssetId } });
 
       File.findOne({where: {name: req.body.file.basic.name, application_id: applicationId}}).then(async (existingFile) => {
         let file = null;
