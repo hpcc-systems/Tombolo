ec0dd5200afe64e6a9c6a3df2580ef2c34edb486
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-04-25 16:22:40 -0700

    Allowing users authenticated with azure to have access to private routes

--- client-reactjs/src/components/common/PrivateRoute.js
+++ client-reactjs/src/components/common/PrivateRoute.js
@@ -1,10 +1,20 @@
+import { useIsAuthenticated } from '@azure/msal-react';
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
-export const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={props => (
-        localStorage.getItem('user')
-            ? <Component {...props} {...rest}/>
-            : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-    )} />
-)
+export const PrivateRoute = ({ component: Component, ...rest }) => {
+  const isAuthenticated = useIsAuthenticated();
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        localStorage.getItem('user') || isAuthenticated ? (
+          <Component {...props} {...rest} />
+        ) : (
+          <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+        )
+      }
+    />
+  );
+};
