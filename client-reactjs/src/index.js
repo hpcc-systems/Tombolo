1790ad37c128133f5dd0a80380da8a52f84ec890
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-04-25 16:18:43 -0700

    adding MSAL provider setup

--- client-reactjs/src/index.js
+++ client-reactjs/src/index.js
@@ -1,25 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {App} from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
-import "./index.css";
+import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/Store';
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "./components/azureSso/azureAuthConfig";
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './components/azureSso/azureAuthConfig';
+import AzureApp from './components/azureSso/AzureApp';
 
 export const msalInstance = new PublicClientApplication(msalConfig);
 
 ReactDOM.render(
   <Provider store={store}>
-      <BrowserRouter>
-      <MsalProvider instance={msalInstance}>
+    <BrowserRouter>
+      {process.env.REACT_APP_APP_AUTH_METHOD === 'azure_ad' ? (
+        <MsalProvider instance={msalInstance}>
+          <AzureApp >
+            <App />
+          </AzureApp>
+        </MsalProvider>
+      ) : (
         <App />
-      </MsalProvider>
-      </BrowserRouter>
+      )}
+    </BrowserRouter>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
