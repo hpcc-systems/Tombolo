299726b2bf64e5b22bd507a685aa918f7928c480
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-04-25 17:04:35 -0700

    fixing config file

--- server/config/azureConfig.js
+++ server/config/azureConfig.js
@@ -1,20 +1,12 @@
-const options = {
+module.exports = {
     identityMetadata: `https://${process.env.AUTHORITY}/${process.env.TENENT_ID}/${ process.env.MSAL_VERSION}/${process.env.DISCOVERY}`,
     issuer: `https://${process.env.AUTHORITY}/${process.env.TENENT_ID}/${process.env.MSAL_VERSION}`,
     clientID: process.env.CLIENT_ID,
     audience: process.env.AUDIENCE,
     validateIssuer: true,
     passReqToCallback: false,
-    loggingLevel: process.env.LOGGING_LEVEL,
-    // scope: EXPOSED_SCOPES,
-    loggingNoPII: false,
-    // Optional -> Default value is false.
-    // Set to true if you accept access_token whose `aud` claim contains multiple values.
-    allowMultiAudiencesInToken: true,
     scope: ['access_as_user'],
-
+    allowMultiAudiencesInToken: true,    // Set to true if you accept access_token whose `aud` claim contains multiple values.
+    // loggingLevel: process.env.LOGGING_LEVEL,
+    // loggingNoPII: false,
   };
-
-  module.exports = {
-      options
-  }
