7bbb19b08625ff755851feda85f07cef99757e98
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-04-26 11:04:30 -0700

    partialy exposing routes on /api/users path

--- server/routes/user/read.js
+++ server/routes/user/read.js
@@ -1,30 +1,99 @@
 const express = require('express');
 const router = express.Router();
 const userService = require('./userservice');
-var models  = require('../../models');
-let User = models.user;
+const tokenService = require('../../utils/token_service')
 
-const { body, query, check, validationResult } = require('express-validator');
-const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
-  return `${msg}`;
-};
-const jwt = require('jsonwebtoken');
-const { JsonWebTokenError } = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
+const errorFormatter = ({ location, msg, param, value, nestedErrors }) => { return `${msg}`; };
+
 // routes
-router.get('/searchuser', searchUser);
-router.post('/authenticate', authenticate);
-router.post('/register', register);
-router.get('/', getAll);
-router.get('/current', getCurrent);
-router.get('/:app_id/sharedAppUser/:username', GetSharedAppUserList);
-router.get('/:id', getById);
-router.put('/:id', update);
-router.delete('/:id', _delete);
-router.post('/validateToken', validateToken);
-router.get('/:user_id/:app_id', GetuserListToShareApp);
-router.post('/changePassword', changePassword);
+router.post('/register', register); // open
+router.post('/authenticate', authenticate); // open
+router.post('/validateToken', validateToken); // open
+router.post('/changePassword', changePassword); // open
+router.get('/probe', (req,res) => res.status(200).end()); // open, polling
+
+router.post('/registerUser', [
+  body('firstName')
+    .matches(/^[a-zA-Z]{1}[a-zA-Z0-9_-]*$/).withMessage('Invalid First Name'),
+  body('lastName').optional({checkFalsy:true})
+    .matches(/^[a-zA-Z]{1}[a-zA-Z0-9_-]*$/).withMessage('Invalid Last Name'),
+  body('username')
+    .matches(/^[a-zA-Z]{1}[a-zA-Z0-9_-]*$/).withMessage('Invalid User Name'),
+  body('email').optional({checkFalsy:true})
+    .isEmail().withMessage('Invalid Email Address'),
+  body('organization').optional({checkFalsy:true})
+    .matches(/^[a-zA-Z]{1}[a-zA-Z0-9_-]*$/).withMessage('Invalid Organization Name'),
+  body('password').optional({checkFalsy:true}).isLength({ min: 4 })
+], (req, res, next) => {
+  const errors = validationResult(req).formatWith(errorFormatter);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ success: false, errors: errors.array() });
+  }
+  userService.registerUser(req, res)
+    .then((response) => {
+      res.status(response.statusCode).json({"success":"true"});
+    })
+    .catch((err) => {
+      res.status(500).json({ errors: err.message });
+    })
+}) // open
+
+router.post('/forgot-password', [
+  body('email')
+    .isEmail().withMessage('Invalid E-mail'),
+], (req, res, next) => {
+  const errors = validationResult(req).formatWith(errorFormatter);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ "success": false, errors: errors.array() });
+  }
+
+  userService.forgotPassword(req, res)
+  .then((response) => {
+    res.status(response.statusCode).json(response.message);
+  })
+  .catch((err) => {
+    // res.status(500).json({ errors: [err.error] });
+    console.log(err);
+  })
+}) // open
+
+router.post('/resetPassword'
+, [
+  body('id')
+  .isLength({ min: 36 }).withMessage('Invalid id'),
+  body('password').optional({checkFalsy:true}).isLength({ min: 4 })
+]
+, (req, res, next) => {
+      const errors = validationResult(req).formatWith(errorFormatter);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ success: false, errors: errors.array() });
+    }
+
+   userService.resetPassword(req, res)
+  .then((response) => {
+    res.status(response.statusCode).json({"success":"true"});
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json({ errors: [err.message] });
+  })
+}) // open
+
+// Authenticate token before proceeding to route
+router.use(tokenService.verifyToken);
+
+router.get('/', getAll); // hidden
+router.put('/:id', update); // hidden 
+router.get('/:id', getById); // hidden
+router.delete('/:id', _delete); // hidden
+router.get('/current', getCurrent); // hidden
+router.get('/searchuser', searchUser); // hidden
+router.get('/:user_id/:app_id', GetuserListToShareApp); // hidden
+router.get('/:app_id/sharedAppUser/:username', GetSharedAppUserList); // hidden
 
 module.exports = router;
+
 function authenticate(req, res, next) {
   //var user = await userService.authenticate(req, res, req.body);
   userService.authenticate(req, res, req.body)
@@ -76,11 +145,13 @@ function validateToken(req, res, next) {
       .then(user => user ? res.json(user.userWithoutHash) : res.status(401).json({ message: 'Invalid Token' }))
       .catch(err => res.status(401).json({ "message": "Invalid Token" }));
 }
+
 function GetuserListToShareApp(req, res, next) {
   userService.GetuserListToShareApp(req, res, next)
       .then(user => user ? res.json(user) : res.sendStatus(404))
       .catch(err => res.status(500).json({ "message": "Error occured while retrieving users" }));
 }
+
 function GetSharedAppUserList(req, res, next) {
   userService.GetSharedAppUserList(req, res, next)
     .then(user => user ? res.json(user) : [])
@@ -100,72 +171,3 @@ function searchUser(req, res, next) {
     .then(users => users ? res.json(users) : res.sendStatus([]))
     .catch(err => res.status(500).json({ "message": "Error occured while searching users" }));
 }
-
-router.post('/registerUser', [
-  body('firstName')
-    .matches(/^[a-zA-Z]{1}[a-zA-Z0-9_-]*$/).withMessage('Invalid First Name'),
-  body('lastName').optional({checkFalsy:true})
-    .matches(/^[a-zA-Z]{1}[a-zA-Z0-9_-]*$/).withMessage('Invalid Last Name'),
-  body('username')
-    .matches(/^[a-zA-Z]{1}[a-zA-Z0-9_-]*$/).withMessage('Invalid User Name'),
-  body('email').optional({checkFalsy:true})
-    .isEmail().withMessage('Invalid Email Address'),
-  body('organization').optional({checkFalsy:true})
-    .matches(/^[a-zA-Z]{1}[a-zA-Z0-9_-]*$/).withMessage('Invalid Organization Name'),
-  body('password').optional({checkFalsy:true}).isLength({ min: 4 })
-], (req, res, next) => {
-  const errors = validationResult(req).formatWith(errorFormatter);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ success: false, errors: errors.array() });
-  }
-  userService.registerUser(req, res)
-    .then((response) => {
-      res.status(response.statusCode).json({"success":"true"});
-    })
-    .catch((err) => {
-      res.status(500).json({ errors: err.message });
-    })
-})
-
-router.post('/forgot-password', [
-  body('email')
-    .isEmail().withMessage('Invalid E-mail'),
-], (req, res, next) => {
-  const errors = validationResult(req).formatWith(errorFormatter);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ "success": false, errors: errors.array() });
-  }
-
-  userService.forgotPassword(req, res)
-  .then((response) => {
-    res.status(response.statusCode).json(response.message);
-  })
-  .catch((err) => {
-    // res.status(500).json({ errors: [err.error] });
-    console.log(err);
-  })
-})
-
-
-
-router.post('/resetPassword'
-, [
-  body('id')
-  .isLength({ min: 36 }).withMessage('Invalid id'),
-  body('password').optional({checkFalsy:true}).isLength({ min: 4 })
-]
-, (req, res, next) => {
-      const errors = validationResult(req).formatWith(errorFormatter);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ success: false, errors: errors.array() });
-    }
-
-   userService.resetPassword(req, res)
-  .then((response) => {
-    res.status(response.statusCode).json({"success":"true"});
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json({ errors: [err.message] });
-  })
-})
