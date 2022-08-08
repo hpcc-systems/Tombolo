16acf3e4a5f149c1525ec48853f52ed35d320625
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-08-08 13:41:46 -0700

    remove unused reducer

--- client-reactjs/src/redux/reducers/index.js
+++ client-reactjs/src/redux/reducers/index.js
@@ -1,16 +1,15 @@
 import { combineReducers } from 'redux';
 import { authenticationReducer } from './AuthReducer';
 import { applicationReducer } from './ApplicationReducer';
-import { dataflowInstancesReducer } from './DataflowInstancesReducer';
 import { dataflowReducer } from './DataflowReducer';
 import { assetReducer } from './AssetReducer';
 import { groupsReducer } from './GroupsReducer';
 import { viewOnlyModeReducer } from './ViewOnlyModeReducer';
 import { groupsMoveReducer } from './GroupsMoveReducer';
+
 const rootReducer = combineReducers({
   authenticationReducer,
   applicationReducer,
-  dataflowInstancesReducer,
   dataflowReducer,
   assetReducer,
   groupsReducer,
