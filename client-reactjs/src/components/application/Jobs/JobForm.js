e387c690d71208a277e1cacc42790c6184f610d2
Kostiantyn Agapov <konstagapov123@gmail.com>
2022-09-29 08:21:44 -0700

    Extract Job Form to separate component

---
+++ client-reactjs/src/components/application/Jobs/JobForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Form } from 'antd';
+import { formItemLayout } from '../../common/CommonUtil';
+import Text from '../../common/Text';
+
+const JobForm = ({ children, inModal, newAsset, colon, onFieldsChange, inTab, jobName, formRef }) => {
+  return (
+    <>
+      {inModal || newAsset ? null : (
+        <div className="assetTitle">
+          <Text text="Job" /> : {jobName}
+        </div>
+      )}
+      <div className={inModal ? 'assetDetails-content-wrapper-modal' : !inTab ? '' : 'assetDetails-content-wrapper'}>
+        <Form
+          colon={colon}
+          {...formItemLayout}
+          initialValues={{
+            selectedFile: null,
+            notify: 'Never',
+            jobType: 'Job',
+            isStoredOnGithub: false,
+          }}
+          labelAlign="left"
+          ref={formRef}
+          scrollToFirstError
+          onFieldsChange={onFieldsChange}>
+          {children}
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default JobForm;
