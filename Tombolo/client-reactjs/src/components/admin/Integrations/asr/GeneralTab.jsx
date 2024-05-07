c414e8a2134db33926135b22e6394819652cc817
yadhap Dahal <yadhap.dahal@lexisnexisrisk.com>
2024-05-07 09:25:37 -0400

    Added UI elements  for capturing NOC severity level and email address

--- Tombolo/client-reactjs/src/components/admin/Integrations/asr/GeneralTab.jsx
+++ Tombolo/client-reactjs/src/components/admin/Integrations/asr/GeneralTab.jsx
@@ -9,6 +9,8 @@ import '../integrations.css';
 function GeneralTab({ integrationDetails, teamsChannels }) {
   const [severity3AlertRecipients, setSeverity3AlertRecipients] = useState(null);
   const [megaphoneAlertRecipients, setMegaphoneAlertRecipients] = useState(null);
+  const [nocAlerts, setNocAlerts] = useState(null);
+  const [minSeverityLevelForNocAlert, setMinSeverityLevelForNocAlert] = useState(null);
 
   // Effects
   useEffect(() => {
@@ -18,92 +20,140 @@ function GeneralTab({ integrationDetails, teamsChannels }) {
       integrationDetails?.appSpecificIntegrationMetaData?.megaPhoneAlerts?.emailContacts || [];
     const megaPhoneTeamsContacts =
       integrationDetails?.appSpecificIntegrationMetaData?.megaPhoneAlerts?.teamsChannel || [];
+    const nocEmailContacts = integrationDetails?.appSpecificIntegrationMetaData?.nocAlerts?.emailContacts || [];
+    const minSeverityLevelForNocAlert =
+      integrationDetails?.appSpecificIntegrationMetaData?.nocAlerts?.severityLevelForNocAlerts || null;
 
     setSeverity3AlertRecipients({ emailContacts: severity3EmailContacts });
     setMegaphoneAlertRecipients({ emailContacts: megaphoneEmailContacts, teamsChannel: megaPhoneTeamsContacts });
+    setNocAlerts({ emailContacts: nocEmailContacts });
+    setMinSeverityLevelForNocAlert(minSeverityLevelForNocAlert);
   }, [integrationDetails]);
 
   // JSX for General Tab
   return (
-    <>
+    <Space direction="vertical">
       <div>
         {integrationDetails?.appSpecificIntegrationMetaData?.severity3Alerts?.active ? (
           <>
-            <Space>
-              <CheckSquareFilled style={{ color: 'var(--primary)' }} />
-              <span>
-                Severity 3 alerts <span style={{ color: 'var(--primary)', fontWeight: '800' }}>ACTIVE</span>
-              </span>
-            </Space>
-            <div>
-              <Card size="small">
-                {severity3AlertRecipients?.emailContacts.map((e, i) => {
-                  return (
-                    <Tag color="blue" key={i}>
-                      {e}
-                    </Tag>
-                  );
-                })}
-              </Card>
-            </div>
+            <Card
+              size="small"
+              title={
+                <Space>
+                  <CheckSquareFilled style={{ color: 'var(--primary)' }} />
+                  <span>
+                    Severity 3 alerts <span style={{ color: 'var(--primary)', fontWeight: '800' }}>ACTIVE</span>
+                  </span>
+                </Space>
+              }>
+              {severity3AlertRecipients?.emailContacts.map((e, i) => {
+                return (
+                  <Tag color="blue" key={i}>
+                    {e}
+                  </Tag>
+                );
+              })}
+            </Card>
           </>
         ) : (
-          <>
+          <Card size="small">
             <Space>
               <CloseSquareFilled style={{ color: 'var(--danger)' }} />
               <span>
                 Severity 3 alerts <span style={{ color: 'var(--danger)', fontWeight: '800' }}>INACTIVE</span>
               </span>
             </Space>
-          </>
+          </Card>
         )}
       </div>
 
       <div>
         {integrationDetails?.appSpecificIntegrationMetaData?.megaPhoneAlerts?.active ? (
           <>
-            <Space>
-              <CheckSquareFilled style={{ color: 'var(--primary)' }} />
-              <span>
-                Megaphone alerts <span style={{ color: 'var(--primary)', fontWeight: '800' }}>ACTIVE</span>
-              </span>
-            </Space>
-            <div>
-              <Card size="small">
-                {megaphoneAlertRecipients?.emailContacts.map((e, i) => {
-                  return (
-                    <Tag color="blue" key={i}>
-                      {e}
-                    </Tag>
-                  );
-                })}
-                {megaphoneAlertRecipients?.teamsChannel.map((t, i) => {
-                  return teamsChannels.map((tc) => {
-                    if (tc.id === t) {
-                      return (
-                        <Tag color="blue" key={i}>
-                          {tc.name}
-                        </Tag>
-                      );
-                    }
-                    return null;
-                  });
-                })}
-              </Card>
-            </div>
+            <Card
+              size="small"
+              title={
+                <Space>
+                  <CheckSquareFilled style={{ color: 'var(--primary)' }} />
+                  <span>
+                    Megaphone alerts <span style={{ color: 'var(--primary)', fontWeight: '800' }}>ACTIVE</span>
+                  </span>
+                </Space>
+              }>
+              {megaphoneAlertRecipients?.emailContacts.map((e, i) => {
+                return (
+                  <Tag color="blue" key={i}>
+                    {e}
+                  </Tag>
+                );
+              })}
+              {megaphoneAlertRecipients?.teamsChannel.map((t, i) => {
+                return teamsChannels.map((tc) => {
+                  if (tc.id === t) {
+                    return (
+                      <Tag color="blue" key={i}>
+                        {tc.name}
+                      </Tag>
+                    );
+                  }
+                  return null;
+                });
+              })}
+            </Card>
           </>
         ) : (
-          <>
+          <Card size="small">
             <Space>
               <CloseSquareFilled style={{ color: 'var(--danger)' }} />
               <span>
                 Megaphone alerts <span style={{ color: 'var(--danger)', fontWeight: '800' }}>INACTIVE</span>
               </span>
             </Space>
-          </>
+          </Card>
         )}
       </div>
-    </>
+
+      <div>
+        {integrationDetails?.appSpecificIntegrationMetaData?.nocAlerts?.active ? (
+          <>
+            <Card
+              size="small"
+              title={
+                <Space>
+                  <CheckSquareFilled style={{ color: 'var(--primary)' }} />
+                  <span>
+                    NOC alerts <span style={{ color: 'var(--primary)', fontWeight: '800' }}>ACTIVE</span> for severity{' '}
+                    {minSeverityLevelForNocAlert < 3 ? (
+                      <span style={{ color: 'var(--primary)', fontWeight: '800' }}>
+                        {minSeverityLevelForNocAlert} and above
+                      </span>
+                    ) : (
+                      ''
+                    )}
+                  </span>
+                </Space>
+              }>
+              {nocAlerts.emailContacts.map((e, i) => {
+                return (
+                  <Tag color="blue" key={i}>
+                    {e}
+                  </Tag>
+                );
+              })}
+            </Card>
+          </>
+        ) : (
+          <Card size="small">
+            <Space>
+              <CloseSquareFilled style={{ color: 'var(--danger)' }} />
+              <span>
+                NOC alerts <span style={{ color: 'var(--danger)', fontWeight: '800' }}>INACTIVE</span>
+              </span>
+            </Space>
+          </Card>
+        )}
+      </div>
+    </Space>
   );
 }
 
