9c42ec05b9f59534ac364f3a7c6dc14a389a22dd
FancMa01 <matthew.fancher@lexisnexisrisk.com>
2024-09-05 14:04:33 -0600

    update sequelize config (#856)
    * update sequelize config
    
    Change sequelize so it stores memory of which migrations and seeders have run in custom named tables
    
    * update package json command

--- Tombolo/server/config/config.js
+++ Tombolo/server/config/config.js
@@ -1,31 +1,34 @@
-const path = require('path');
-const fs = require('fs');
-const rootENV = path.join(process.cwd(), '..', '.env');
-const serverENV = path.join(process.cwd(), '.env');
+const path = require("path");
+const fs = require("fs");
+const rootENV = path.join(process.cwd(), "..", ".env");
+const serverENV = path.join(process.cwd(), ".env");
 const ENVPath = fs.existsSync(rootENV) ? rootENV : serverENV;
-require('dotenv').config({ path: ENVPath});
+require("dotenv").config({ path: ENVPath });
 
-const logger = require('./logger');
+const logger = require("./logger");
 const dbConfigOptions = {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: 'mysql',
-    seederStorage: 'json',
-    logging: (msg) => logger.debug(msg), // change winston settings to 'debug' to see this log
-}
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOSTNAME,
+  dialect: "mysql",
+  seederStorage: "sequelize",
+  seederStorageTableName: "sequelize_seeders",
+  migrationStorageTableName: "sequelize_migrations",
 
-if(process.env.MYSQL_SSL_ENABLED === "true"){
-   dbConfigOptions.ssl = true;
-   dbConfigOptions.dialectOptions = {
-      ssl: {
-          require: true
-       }
-   }
+  logging: (msg) => logger.debug(msg), // change winston settings to 'debug' to see this log
+};
+
+if (process.env.MYSQL_SSL_ENABLED === "true") {
+  dbConfigOptions.ssl = true;
+  dbConfigOptions.dialectOptions = {
+    ssl: {
+      require: true,
+    },
+  };
 }
 
 module.exports = {
   development: dbConfigOptions,
-  production: dbConfigOptions
+  production: dbConfigOptions,
 };
