bff108eb0ed96fc432fd24dbbf3b075d30c0a0c4
FancMa01 <matthew.fancher@lexisnexisrisk.com>
2024-10-10 10:21:22 -0600

    Make change password functional (#907)

--- Tombolo/server/routes/userRoutes.js
+++ Tombolo/server/routes/userRoutes.js
@@ -13,7 +13,6 @@ const {
   validatePatchUserRolesPayload,
 } = require("../middlewares/userMiddleware");
 
-
 // Import user controller
 const {
   deleteUser,
@@ -25,23 +24,41 @@ const {
   bulkUpdateUsers,
   updateUserRoles,
 } = require("../controllers/userController");
+
 const { validateUserRole } = require("../middlewares/rbacMiddleware");
 
-
 // TODO - Add guards so only users can change their own password
-router.patch("/change-password/:id", validateUserId, validateChangePasswordPayload, changePassword); // Change password
+router.patch(
+  "/change-password/:id",
+  validateUserId,
+  validateChangePasswordPayload,
+  changePassword
+); // Change password
+router.patch(
+  "/:id",
+  validateUserId,
+  validateUpdateUserPayload,
+  updateBasicUserInfo
+); // Update a user by id
 
-// All routes below is accessible only by users with role "owner" and "administrator"
-router.use(validateUserRole([role.OWNER, role.ADMIN]));
-
+router.use(validateUserRole([role.OWNER, role.ADMIN])); // All routes below this line will require the user to be an owner or admin
 // Routes
 router.get("/", getAllUsers); // Get all users
-router.get("/:id", validateUserId, getUser); // Get a user by id
-router.delete("/bulk-delete", validateBulkDeletePayload, bulkDeleteUsers); // Bulk delete users
-router.delete("/:id",  validateUserId, deleteUser); // Delete a user by id
-router.patch("/bulk-update",  validateBulkUpdatePayload, bulkUpdateUsers); // Bulk update users
-router.patch("/:id",  validateUserId, validateUpdateUserPayload, updateBasicUserInfo); // Update a user by id
-router.patch("/roles/update/:id",  validateUserId, validatePatchUserRolesPayload, updateUserRoles); // Update a user by id
+router.get(
+  "/:id",
+  validateUserId,
 
-//Export 
+  getUser
+); // Get a user by id
+router.delete("/bulk-delete", validateBulkDeletePayload, bulkDeleteUsers); // Bulk delete users
+router.delete("/:id", validateUserId, deleteUser); // Delete a user by id
+router.patch("/bulk-update", validateBulkUpdatePayload, bulkUpdateUsers); // Bulk update users
+router.patch(
+  "/roles/update/:id",
+  validateUserId,
+  validatePatchUserRolesPayload,
+  updateUserRoles
+); // Update a user by id
+
+//Export
 module.exports = router;
