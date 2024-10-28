40a3b6f1483ce6f6ef0a29bc8712f8558984c594
Matt Fancher <142915944+FancMa01@users.noreply.github.com>
2024-10-28 08:18:10 -0600

    Model and Migration

---
+++ Tombolo/server/models/instance_settings.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const { create } = require("lodash");
+
+module.exports = (sequelize, DataTypes) => {
+  const instance_settings = sequelize.define(
+    "instance_settings",
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      value: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      metaData: {
+        type: DataTypes.JSON,
+        allowNull: true,
+      },
+      createdBy: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      updatedBy: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      deletedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    { freezeTableName: true },
+    {
+      tableName: "instance_settings",
+    }
+  );
+
+  return instance_settings;
+};
