3036e828dc5af210af688fa760d8051e2e513197
Matt Fancher <142915944+FancMa01@users.noreply.github.com>
2024-12-10 08:31:02 -0700

    Update middleware to reflect production env variable code

--- Tombolo/server/middlewares/csrfMiddleware.js
+++ Tombolo/server/middlewares/csrfMiddleware.js
@@ -4,10 +4,10 @@ const csrf = doubleCsrf({
   getSecret: () => process.env.CSRF_SECRET, // THIS IS A NAIVE DOUBLE CSRF IMPLEMENTATION, NEED TO IMPLEMENT A ROTATING KEY OF SOME KIND OR USER IDENTIFYING KEY
   getTokenFromRequest: (req) => req.headers["x-csrf-token"], // A function that returns the token from the request
 
-  cookieName: "x-csrf-token",
-  // process.env.NODE_ENV === "production"
-  //   ? "__Host-prod.x-csrf-token"
-  //   : "_csrf",
+  cookieName:
+    process.env.NODE_ENV === "production"
+      ? "__Host-prod.x-csrf-token"
+      : "x-csrf-token",
   cookieOptions: {
     secure: process.env.NODE_ENV === "production", // Enable for HTTPS in production
     httpOnly: false, //client needs to be able to read and set the cookie
