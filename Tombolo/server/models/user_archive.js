c10ac8bc405622fee2dc0a4085be4d4896c26ad1
Matt Fancher <142915944+FancMa01@users.noreply.github.com>
2025-02-20 08:29:40 -0700

    Create user archive table
    Created user archive table with all of the same columns of User, without Hash, adding in removed By and removed At. This table will be used to hold deleted users

---
+++ Tombolo/server/models/user_archive.js
@@ -0,0 +1,95 @@
+"use strict";
+
+module.exports = (sequelize, DataTypes) => {
+  const userArchive = sequelize.define(
+    "userArchive",
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      removedBy: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      removedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      hash: {
+        type: DataTypes.STRING,
+        allowNull: true, // For OAuth users - no password is stored
+      },
+      registrationMethod: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [["traditional", "azure"]],
+        },
+      },
+      verifiedUser: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      verifiedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      registrationStatus: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "pending",
+        validate: {
+          isIn: [["pending", "active", "revoked"]], // Must be one of these values
+        },
+      },
+      forcePasswordReset: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      passwordExpiresAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      lastLoginAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      lastAccessedAt: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      metaData: {
+        type: DataTypes.JSON,
+        defaultValue: {}, //put default value to avoid null conflicts
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "user_archive",
+      timestamps: true,
+    }
+  );
+
+  return userArchive;
+};
