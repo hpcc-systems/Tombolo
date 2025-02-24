bb2d21688a3ce5973bcc15779e6f9469417b56a6
FancMa01 <matthew.fancher@lexisnexisrisk.com>
2025-02-24 08:11:59 -0700

    Mfancher/archive user table (#1024)
    * Fixes and change model and migration for user to add column
    
    * set last login whenever logins or password resets occur
    
    * Remove Inactive accounts
    
    This sets up a job that runs every 12 hours to delete user accounts that are over 180 days since last login
    
    * clarify messaging
    
    * Fix Messaging and Remove Admin email since workflow is no longer required
    
    * rename function to clarify
    
    * Create user archive table
    
    Created user archive table with all of the same columns of User, without Hash, adding in removed By and removed At. This table will be used to hold deleted users
    
    * Update user_archive.js
    
    * remove unique email constraint on archive

---
+++ Tombolo/server/migrations/20250219847293-create-user_archive.js
@@ -0,0 +1,97 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("user_archive", {
+      id: {
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      },
+      removedBy: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      removedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      registrationMethod: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [["traditional", "microsoft"]],
+        },
+      },
+      verifiedUser: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      verifiedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      registrationStatus: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          isIn: [["pending", "active", "revoked"]],
+        },
+      },
+      forcePasswordReset: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      passwordExpiresAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      lastLoginAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      lastAccessedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      metaData: {
+        type: Sequelize.JSON,
+        defaultValue: {},
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("users");
+  },
+};
