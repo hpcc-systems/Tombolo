d10b6330bfc21ae724a1c0b6043db4f042c99f49
Matt Fancher <142915944+FancMa01@users.noreply.github.com>
2025-03-07 13:09:25 -0700

    fix

--- Tombolo/server/models/jobMonitoring_data.js
+++ Tombolo/server/models/jobMonitoring_data.js
@@ -1,5 +1,58 @@
 "use strict";
-const JobMonitoringData = (module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize, DataTypes) => {
+  const JobMonitoringData = sequelize.define(
+    "jobMonitoring_Data",
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      monitoringId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: "jobMonitoring",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+      date: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      metaData: {
+        allowNull: false,
+        type: DataTypes.JSONB,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      tableName: "jobMonitoring_Data",
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+  JobMonitoringData.associate = function (models) {
+    JobMonitoringData.belongsTo(models.application, {
+      foreignKey: "jobMonitoring",
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    });
+  };
   const timeSeriesAnalysisAndAlert = (data) => {
     const jobMonitoringData = sequelize.models.jobMonitoring_Data;
     const notificationQueue = sequelize.models.notificationQueue;
@@ -75,63 +128,9 @@ const JobMonitoringData = (module.exports = (sequelize, DataTypes) => {
 
     return;
   };
-  const JobMonitoringData = sequelize.define(
-    "jobMonitoring_Data",
-    {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-      },
-      monitoringId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        references: {
-          model: "jobMonitoring",
-          key: "id",
-        },
-        onUpdate: "CASCADE",
-        onDelete: "CASCADE",
-      },
-      date: {
-        allowNull: false,
-        type: DataTypes.DATE,
-      },
-      metaData: {
-        allowNull: false,
-        type: DataTypes.JSONB,
-      },
-      createdAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-      },
-      deletedAt: {
-        allowNull: true,
-        type: DataTypes.DATE,
-      },
-    },
-    {
-      tableName: "jobMonitoring_Data",
-      timestamps: true,
-      paranoid: true,
-    }
-  );
-  JobMonitoringData.associate = function (models) {
-    JobMonitoringData.belongsTo(models.application, {
-      foreignKey: "jobMonitoring",
-      onDelete: "CASCADE",
-      onUpdate: "CASCADE",
-    });
-  };
   JobMonitoringData.addHook("afterCreate", async (instance, options) => {
     timeSeriesAnalysisAndAlert({ data: instance });
   });
 
   return JobMonitoringData;
-});
-module.exports = JobMonitoringData;
+};
