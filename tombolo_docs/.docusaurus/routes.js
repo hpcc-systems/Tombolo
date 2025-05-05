8d69093def20485584e595d1ae85b504bd661493
Yadhap <yadhapdahal111@gmail.com>
2025-05-05 16:58:39 -0400

    changed doc dir name

--- tombolo_docs/.docusaurus/routes.js
+++ tombolo_docs/.docusaurus/routes.js
@@ -3,243 +3,243 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/ping_pong/blog',
-    component: ComponentCreator('/ping_pong/blog', 'ae8'),
+    path: '/tombolo/blog',
+    component: ComponentCreator('/tombolo/blog', 'a37'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/archive',
-    component: ComponentCreator('/ping_pong/blog/archive', '207'),
+    path: '/tombolo/blog/archive',
+    component: ComponentCreator('/tombolo/blog/archive', '183'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/authors',
-    component: ComponentCreator('/ping_pong/blog/authors', '575'),
+    path: '/tombolo/blog/authors',
+    component: ComponentCreator('/tombolo/blog/authors', '194'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/authors/all-sebastien-lorber-articles',
-    component: ComponentCreator('/ping_pong/blog/authors/all-sebastien-lorber-articles', '96e'),
+    path: '/tombolo/blog/authors/all-sebastien-lorber-articles',
+    component: ComponentCreator('/tombolo/blog/authors/all-sebastien-lorber-articles', '697'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/authors/yangshun',
-    component: ComponentCreator('/ping_pong/blog/authors/yangshun', '79b'),
+    path: '/tombolo/blog/authors/yangshun',
+    component: ComponentCreator('/tombolo/blog/authors/yangshun', 'c57'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/first-blog-post',
-    component: ComponentCreator('/ping_pong/blog/first-blog-post', '5df'),
+    path: '/tombolo/blog/first-blog-post',
+    component: ComponentCreator('/tombolo/blog/first-blog-post', '630'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/long-blog-post',
-    component: ComponentCreator('/ping_pong/blog/long-blog-post', '86d'),
+    path: '/tombolo/blog/long-blog-post',
+    component: ComponentCreator('/tombolo/blog/long-blog-post', '256'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/mdx-blog-post',
-    component: ComponentCreator('/ping_pong/blog/mdx-blog-post', 'df4'),
+    path: '/tombolo/blog/mdx-blog-post',
+    component: ComponentCreator('/tombolo/blog/mdx-blog-post', '6c6'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/tags',
-    component: ComponentCreator('/ping_pong/blog/tags', '372'),
+    path: '/tombolo/blog/tags',
+    component: ComponentCreator('/tombolo/blog/tags', '485'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/tags/docusaurus',
-    component: ComponentCreator('/ping_pong/blog/tags/docusaurus', '42d'),
+    path: '/tombolo/blog/tags/docusaurus',
+    component: ComponentCreator('/tombolo/blog/tags/docusaurus', 'af9'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/tags/facebook',
-    component: ComponentCreator('/ping_pong/blog/tags/facebook', 'b3d'),
+    path: '/tombolo/blog/tags/facebook',
+    component: ComponentCreator('/tombolo/blog/tags/facebook', 'ed5'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/tags/hello',
-    component: ComponentCreator('/ping_pong/blog/tags/hello', '993'),
+    path: '/tombolo/blog/tags/hello',
+    component: ComponentCreator('/tombolo/blog/tags/hello', '1da'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/tags/hola',
-    component: ComponentCreator('/ping_pong/blog/tags/hola', '72c'),
+    path: '/tombolo/blog/tags/hola',
+    component: ComponentCreator('/tombolo/blog/tags/hola', '6e3'),
     exact: true
   },
   {
-    path: '/ping_pong/blog/welcome',
-    component: ComponentCreator('/ping_pong/blog/welcome', '4c8'),
+    path: '/tombolo/blog/welcome',
+    component: ComponentCreator('/tombolo/blog/welcome', '3d2'),
     exact: true
   },
   {
-    path: '/ping_pong/faq',
-    component: ComponentCreator('/ping_pong/faq', '792'),
+    path: '/tombolo/faq',
+    component: ComponentCreator('/tombolo/faq', 'b72'),
     exact: true
   },
   {
-    path: '/ping_pong/markdown-page',
-    component: ComponentCreator('/ping_pong/markdown-page', '8d0'),
+    path: '/tombolo/markdown-page',
+    component: ComponentCreator('/tombolo/markdown-page', '25e'),
     exact: true
   },
   {
-    path: '/ping_pong/release-notes',
-    component: ComponentCreator('/ping_pong/release-notes', '310'),
+    path: '/tombolo/release-notes',
+    component: ComponentCreator('/tombolo/release-notes', '07b'),
     exact: true
   },
   {
-    path: '/ping_pong/docs',
-    component: ComponentCreator('/ping_pong/docs', '4b0'),
+    path: '/tombolo/docs',
+    component: ComponentCreator('/tombolo/docs', '046'),
     routes: [
       {
-        path: '/ping_pong/docs',
-        component: ComponentCreator('/ping_pong/docs', '96b'),
+        path: '/tombolo/docs',
+        component: ComponentCreator('/tombolo/docs', '9a7'),
         routes: [
           {
-            path: '/ping_pong/docs',
-            component: ComponentCreator('/ping_pong/docs', '0fc'),
+            path: '/tombolo/docs',
+            component: ComponentCreator('/tombolo/docs', '98a'),
             routes: [
               {
-                path: '/ping_pong/docs/category/developer-resources',
-                component: ComponentCreator('/ping_pong/docs/category/developer-resources', 'dfb'),
+                path: '/tombolo/docs/category/developer-resources',
+                component: ComponentCreator('/tombolo/docs/category/developer-resources', '3d2'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/category/installation--configuration',
-                component: ComponentCreator('/ping_pong/docs/category/installation--configuration', 'e6d'),
+                path: '/tombolo/docs/category/installation--configuration',
+                component: ComponentCreator('/tombolo/docs/category/installation--configuration', 'd80'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/category/monitoring',
-                component: ComponentCreator('/ping_pong/docs/category/monitoring', '183'),
+                path: '/tombolo/docs/category/monitoring',
+                component: ComponentCreator('/tombolo/docs/category/monitoring', 'dd0'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/category/user-guides',
-                component: ComponentCreator('/ping_pong/docs/category/user-guides', '374'),
+                path: '/tombolo/docs/category/user-guides',
+                component: ComponentCreator('/tombolo/docs/category/user-guides', '939'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/Developer/Integrations',
-                component: ComponentCreator('/ping_pong/docs/Developer/Integrations', '28d'),
+                path: '/tombolo/docs/Developer/Integrations',
+                component: ComponentCreator('/tombolo/docs/Developer/Integrations', 'a6e'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/Developer/Testing',
-                component: ComponentCreator('/ping_pong/docs/Developer/Testing', '58a'),
+                path: '/tombolo/docs/Developer/Testing',
+                component: ComponentCreator('/tombolo/docs/Developer/Testing', '4fd'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/Install/Configurations',
-                component: ComponentCreator('/ping_pong/docs/Install/Configurations', '3d8'),
+                path: '/tombolo/docs/Install/Configurations',
+                component: ComponentCreator('/tombolo/docs/Install/Configurations', 'c9b'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/Install/Docker',
-                component: ComponentCreator('/ping_pong/docs/Install/Docker', '94f'),
+                path: '/tombolo/docs/Install/Docker',
+                component: ComponentCreator('/tombolo/docs/Install/Docker', '94f'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/Install/Local',
-                component: ComponentCreator('/ping_pong/docs/Install/Local', '91e'),
+                path: '/tombolo/docs/Install/Local',
+                component: ComponentCreator('/tombolo/docs/Install/Local', 'dcb'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/Install/Uninstall',
-                component: ComponentCreator('/ping_pong/docs/Install/Uninstall', '176'),
+                path: '/tombolo/docs/Install/Uninstall',
+                component: ComponentCreator('/tombolo/docs/Install/Uninstall', '7ce'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/application',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/application', 'b32'),
+                path: '/tombolo/docs/User-Guides/application',
+                component: ComponentCreator('/tombolo/docs/User-Guides/application', 'd3d'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/assets',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/assets', '313'),
+                path: '/tombolo/docs/User-Guides/assets',
+                component: ComponentCreator('/tombolo/docs/User-Guides/assets', '3bd'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/cluster',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/cluster', '7be'),
+                path: '/tombolo/docs/User-Guides/cluster',
+                component: ComponentCreator('/tombolo/docs/User-Guides/cluster', '114'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/dashboards',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/dashboards', '11e'),
+                path: '/tombolo/docs/User-Guides/dashboards',
+                component: ComponentCreator('/tombolo/docs/User-Guides/dashboards', '2f4'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/github',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/github', '956'),
+                path: '/tombolo/docs/User-Guides/github',
+                component: ComponentCreator('/tombolo/docs/User-Guides/github', 'ead'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/monitoring/ClusterMonitoring',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/monitoring/ClusterMonitoring', 'f9e'),
+                path: '/tombolo/docs/User-Guides/monitoring/ClusterMonitoring',
+                component: ComponentCreator('/tombolo/docs/User-Guides/monitoring/ClusterMonitoring', '7d6'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/monitoring/DirectoryMonitoring',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/monitoring/DirectoryMonitoring', '066'),
+                path: '/tombolo/docs/User-Guides/monitoring/DirectoryMonitoring',
+                component: ComponentCreator('/tombolo/docs/User-Guides/monitoring/DirectoryMonitoring', '7ee'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/monitoring/jobMonitoring',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/monitoring/jobMonitoring', '771'),
+                path: '/tombolo/docs/User-Guides/monitoring/jobMonitoring',
+                component: ComponentCreator('/tombolo/docs/User-Guides/monitoring/jobMonitoring', '457'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/monitoring/SuperFilesMonitoring',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/monitoring/SuperFilesMonitoring', '6a9'),
+                path: '/tombolo/docs/User-Guides/monitoring/SuperFilesMonitoring',
+                component: ComponentCreator('/tombolo/docs/User-Guides/monitoring/SuperFilesMonitoring', '9e2'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/teams-webhook',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/teams-webhook', 'bce'),
+                path: '/tombolo/docs/User-Guides/teams-webhook',
+                component: ComponentCreator('/tombolo/docs/User-Guides/teams-webhook', '700'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/user-interface',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/user-interface', '38b'),
+                path: '/tombolo/docs/User-Guides/user-interface',
+                component: ComponentCreator('/tombolo/docs/User-Guides/user-interface', '5cc'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/users',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/users', '7b7'),
+                path: '/tombolo/docs/User-Guides/users',
+                component: ComponentCreator('/tombolo/docs/User-Guides/users', '1eb'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/Wildcards',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/Wildcards', 'aa4'),
+                path: '/tombolo/docs/User-Guides/Wildcards',
+                component: ComponentCreator('/tombolo/docs/User-Guides/Wildcards', '230'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               },
               {
-                path: '/ping_pong/docs/User-Guides/workflows',
-                component: ComponentCreator('/ping_pong/docs/User-Guides/workflows', '350'),
+                path: '/tombolo/docs/User-Guides/workflows',
+                component: ComponentCreator('/tombolo/docs/User-Guides/workflows', '8aa'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               }
@@ -250,8 +250,8 @@ export default [
     ]
   },
   {
-    path: '/ping_pong/',
-    component: ComponentCreator('/ping_pong/', '540'),
+    path: '/tombolo/',
+    component: ComponentCreator('/tombolo/', '70f'),
     exact: true
   },
   {
