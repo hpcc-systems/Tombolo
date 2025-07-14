9bc84f97b28dc4a2dd48e83e0e10533608e3bcd6
sodon-lnr <sean.odonnell@lexisnexisrisk.com>
2025-07-14 12:08:30 -0400

    Added type declaration to migration for better type inferencing.

--- Tombolo/server/migrations/20250630000000-create-landingZoneMonitoring-table.js
+++ Tombolo/server/migrations/20250630000000-create-landingZoneMonitoring-table.js
@@ -1,4 +1,6 @@
 'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('landingZoneMonitoring', {
