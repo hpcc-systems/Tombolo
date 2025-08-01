ad0ccb75fb4037de318cf19bce230c6c0e91a045
Yadhap <yadhapdahal111@gmail.com>
2025-08-01 16:09:12 -0400

    Updated the cluster monitoring migration and model. renamed table and added addation column

--- Tombolo/server/models/clusterMonitoring.js
+++ Tombolo/server/models/clusterMonitoring.js
@@ -1,61 +1,165 @@
 'use strict';
+
+const { validate } = require('uuid');
+
 module.exports = (sequelize, DataTypes) => {
-  const clusterMonitoring = sequelize.define(
-    'clusterMonitoring',
+  const ClusterMonitoring = sequelize.define(
+    'cluster_monitoring',
     {
       id: {
+        allowNull: false,
         primaryKey: true,
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
-        allowNull: false,
-        autoIncrement: false,
       },
-      name: {
+      monitoringName: {
         allowNull: false,
         type: DataTypes.STRING,
+        unique: true,
       },
-      cron: {
-        allowNull: true,
-        type: DataTypes.STRING,
-      },
-      application_id: {
+      clusterMonitoringType: {
         allowNull: false,
-        type: DataTypes.UUID,
-      },
-      cluster_id: {
-        type: DataTypes.UUID,
-        allowNull: false,
-      },
-      metaData: {
         type: DataTypes.JSON,
-        allowNull: true,
+        defaultValue: ['status', 'usage'],
       },
       isActive: {
+        allowNull: false,
+        defaultValue: false,
         type: DataTypes.BOOLEAN,
+      },
+      approvalStatus: {
+        allowNull: false,
+        type: DataTypes.ENUM('approved', 'rejected', 'pending'),
+        defaultValue: 'pending',
+      },
+      approvedBy: {
         allowNull: true,
+        type: DataTypes.UUID,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'NO ACTION',
+      },
+      approvedAt: {
+        allowNull: true,
+        type: DataTypes.DATE,
+      },
+      approverComment: {
+        allowNull: true,
+        type: DataTypes.STRING,
+      },
+      description: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      clusterId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'cluster',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      lastRunDetails: {
+        allowNull: true,
+        type: DataTypes.JSON,
+      },
+      metaData: {
+        allowNull: false,
+        type: DataTypes.JSON,
+        defaultValue: {
+          contacts: {
+            primaryContacts: [],
+            secondaryContacts: [],
+            notifyContacts: [],
+          },
+          monitoringData: {},
+          asrSpecificMetaData: {},
+        },
+      },
+      createdBy: {
+        allowNull: false,
+        type: DataTypes.UUID,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'NO ACTION',
+      },
+      lastUpdatedBy: {
+        allowNull: false,
+        type: DataTypes.UUID,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'NO ACTION',
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: DataTypes.DATE,
+      },
+      deletedBy: {
+        allowNull: true,
+        type: DataTypes.UUID,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'NO ACTION',
       },
     },
     {
       paranoid: true,
       freezeTableName: true,
-      indexes: [
-        {
-          unique: true,
-          fields: ['name', 'deletedAt'],
-        },
-      ],
     }
   );
-  clusterMonitoring.associate = function (models) {
-    // Define association here
-    clusterMonitoring.belongsTo(models.cluster, { foreignKey: 'cluster_id' });
-    clusterMonitoring.belongsTo(models.application, {
-      foreignKey: 'application_id',
-    });
-    clusterMonitoring.hasMany(models.monitoring_notifications, {
-      foreignKey: 'application_id',
+
+  // Associations
+  ClusterMonitoring.associate = function (models) {
+    ClusterMonitoring.belongsTo(models.cluster, {
+      foreignKey: 'clusterId',
       onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+
+    // User associations
+    ClusterMonitoring.belongsTo(models.user, {
+      foreignKey: 'createdBy',
+      as: 'creator',
+      onDelete: 'NO ACTION',
+      onUpdate: 'CASCADE',
+    });
+
+    ClusterMonitoring.belongsTo(models.user, {
+      foreignKey: 'lastUpdatedBy',
+      as: 'updater',
+      onDelete: 'NO ACTION',
+      onUpdate: 'CASCADE',
+    });
+
+    ClusterMonitoring.belongsTo(models.user, {
+      foreignKey: 'approvedBy',
+      as: 'approver',
+      onDelete: 'NO ACTION',
+      onUpdate: 'CASCADE',
     });
   };
-  return clusterMonitoring;
+
+  return ClusterMonitoring;
 };
