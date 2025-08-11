fd8e4a01bb94ecd62b8c6ab7b6670433135fd118
sodon-lnr <sean.odonnell@lexisnexisrisk.com>
2025-08-11 13:30:37 -0400

    Commented out old redux reducer index that called the old package. Uninstalled redux-thunk

--- Tombolo/client-reactjs/src/redux/reducers/index.js
+++ Tombolo/client-reactjs/src/redux/reducers/index.js
@@ -1,20 +1,20 @@
-import { combineReducers } from 'redux';
-import { authenticationReducer } from './AuthReducer';
-import { applicationReducer } from './ApplicationReducer';
-import { dataflowReducer } from './DataflowReducer';
-import { assetReducer } from './AssetReducer';
-import { groupsReducer } from './GroupsReducer';
-import { propagationReducer } from './PropagationReducer';
-import { backendReducer } from './BackendReducer';
+// import { combineReducers } from 'redux';
+// import { authenticationReducer } from './AuthReducer';
+// import { applicationReducer } from './ApplicationReducer';
+// import { dataflowReducer } from './DataflowReducer';
+// import { assetReducer } from './AssetReducer';
+// import { groupsReducer } from './GroupsReducer';
+// import { propagationReducer } from './PropagationReducer';
+// import { backendReducer } from './BackendReducer';
+//
+// const rootReducer = combineReducers({
+//   authenticationReducer,
+//   applicationReducer,
+//   backendReducer,
+//   dataflowReducer,
+//   assetReducer,
+//   groupsReducer,
+//   propagation: propagationReducer,
+// });
 
-const rootReducer = combineReducers({
-  authenticationReducer,
-  applicationReducer,
-  backendReducer,
-  dataflowReducer,
-  assetReducer,
-  groupsReducer,
-  propagation: propagationReducer,
-});
-
-export default rootReducer;
+// export default rootReducer;
