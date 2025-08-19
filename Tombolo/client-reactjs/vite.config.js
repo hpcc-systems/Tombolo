f0bd5e1768c4ed2cc50d9df0e2e117bc34a21f41
sodon-lnr <sean.odonnell@lexisnexisrisk.com>
2025-08-19 14:47:35 -0400

    Added antd packages to be included in optimizeDeps. Forcing prebundle to esm for internal packages that were commonjs.

--- Tombolo/client-reactjs/vite.config.js
+++ Tombolo/client-reactjs/vite.config.js
@@ -18,6 +18,17 @@ export default function config({ mode }) {
         },
       }),
     ],
+    optimizeDeps: {
+      include: [
+        '@ant-design/plots',
+        '@ant-design/charts',
+        '@antv/g2',
+        '@antv/component',
+        '@antv/coord',
+        '@antv/scale',
+        'color-string',
+      ],
+    },
     css: {
       modules: {
         localsConvention: 'camelCase',
