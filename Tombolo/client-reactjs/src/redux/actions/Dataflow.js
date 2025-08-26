bf08420e5c9ae0dcd788a591629b9cf76978f570
sodon-lnr <sean.odonnell@lexisnexisrisk.com>
2025-08-26 09:04:16 -0400

    Removed old actions and reducers.

--- Tombolo/client-reactjs/src/redux/actions/Dataflow.js
+++
@@ -1,6 +0,0 @@
-import { Constants } from '../../components/common/Constants';
-
-export const dataflowAction = {
-  dataflowSelected: (dataflow) => ({ type: Constants.DATAFLOW_SELECTED, payload: dataflow }),
-  dataflowReset: () => ({ type: Constants.DATAFLOW_RESET }),
-};
