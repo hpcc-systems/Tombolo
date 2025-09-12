85d5e83063d7a2f9ce1123be4d3d70109df98a17
sodon-lnr <sean.odonnell@lexisnexisrisk.com>
2025-09-12 13:56:50 -0400

    Created unit tests for two common components and ensured configs were correct. Fixed a bug found when writing the tests.

--- Tombolo/client-reactjs/src/tests/setupTests.js
+++ Tombolo/client-reactjs/src/tests/setupTests.js
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom';
+
+// Ant Design relies on matchMedia for responsive behavior. Polyfill for JSDOM.
+if (!window.matchMedia) {
+  window.matchMedia = (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {}, // deprecated
+    removeListener: () => {}, // deprecated
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  });
+}
