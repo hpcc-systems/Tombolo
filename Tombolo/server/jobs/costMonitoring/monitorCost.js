20ff19cbc7b45daa8be318be3c257ec629d0389c
sodon-lnr <sean.odonnell@lexisnexisrisk.com>
2025-09-24 15:29:58 -0400

    Created a getClusters method to allow for retrieving multiple clusters at once and checking credentials and reachability asynchronously. Implemented it in monitorCost.

--- Tombolo/server/jobs/costMonitoring/monitorCost.js
+++ Tombolo/server/jobs/costMonitoring/monitorCost.js
@@ -3,12 +3,11 @@ const { parentPort } = require('worker_threads');
 const {
   CostMonitoring,
   CostMonitoringData,
-  Cluster,
   MonitoringLog,
   MonitoringType,
 } = require('../../models');
 const { Workunit } = require('@hpcc-js/comms');
-const { getCluster } = require('../../utils/hpcc-util');
+const { getClusters } = require('../../utils/hpcc-util');
 const { getClusterOptions } = require('../../utils/getClusterOptions');
 
 /**
@@ -165,34 +164,26 @@ async function monitorCost() {
 
     // 1. Gather all unique cluster IDs referenced by any cost monitoring (or all clusters if any monitoring has clusterIds null/empty)
     let allClusterIds = new Set();
-    let useAllClusters = false;
     for (const costMonitor of costMonitorings) {
-      if (!costMonitor.clusterIds || costMonitor.clusterIds.length === 0) {
-        useAllClusters = true;
-        break;
-      }
       costMonitor.clusterIds.forEach(id => allClusterIds.add(id));
     }
 
-    let clusterRecords;
-    if (useAllClusters) {
-      clusterRecords = await Cluster.findAll({
-        attributes: ['id'],
-        where: { deletedAt: null },
-      });
-      allClusterIds = new Set(clusterRecords.map(c => c.id));
-    } else {
-      clusterRecords = await Cluster.findAll({
-        attributes: ['id'],
-        where: { id: Array.from(allClusterIds), deletedAt: null },
-      });
-    }
+    const clusterDetails = await getClusters(Array.from(allClusterIds));
 
     // 2. For each cluster, aggregate all cost monitoring data into a single CostMonitoringData row
-    for (const clusterRecord of clusterRecords) {
-      const clusterId = clusterRecord.id;
+    for (const clusterDetail of clusterDetails) {
+      const clusterId = clusterDetail.id;
+      // TODO: Call logOrPost here
+      // Check if the cluster had an errors during getClusters
+      if ('error' in clusterDetail) {
+        parentPort &&
+          parentPort.postMessage({
+            level: 'error',
+            text: `monitorCost: Failed to get cluster ${clusterId}: ${clusterDetail.error}, ...skipping`,
+          });
+        continue;
+      }
       try {
-        const clusterDetail = await getCluster(clusterId);
         const {
           thor_host: thorHost,
           thor_port: thorPort,
