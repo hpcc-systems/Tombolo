dbf2d5b1e3bc2b73277ba5cd8367fa0b0ed6c572
Yadhap <yadhapdahal111@gmail.com>
2025-11-14 10:35:57 -0500

    Added service layer for Orbit Profile Monitoring

---
+++ Tombolo/client-reactjs/src/services/orbitProfileMonitoring.service.js
@@ -0,0 +1,45 @@
+import { apiClient } from '@/services/api';
+
+const orbitProfileMonitoringService = {
+  create: async (data, applicationId) => {
+    const response = await apiClient.post(`/orbitProfileMonitoring/${applicationId}`, data);
+    return response.data;
+  },
+
+  getAll: async applicationId => {
+    const response = await apiClient.get(`/orbitProfileMonitoring/getAll/${applicationId}`);
+    return response.data;
+  },
+
+  getById: async id => {
+    const response = await apiClient.get(`/orbitProfileMonitoring/getOne/${id}`);
+    return response.data;
+  },
+
+  updateOne: async (id, updatedData) => {
+    const response = await apiClient.put(`/orbitProfileMonitoring/${id}`, updatedData);
+    return response.data;
+  },
+
+  delete: async ids => {
+    const response = await apiClient.delete('/orbitProfileMonitoring/', {
+      data: { ids },
+    });
+    return response.data;
+  },
+
+  toggleStatus: async (ids, isActive) => {
+    const response = await apiClient.patch('/orbitProfileMonitoring/toggleStatus', {
+      ids,
+      isActive,
+    });
+    return response.data;
+  },
+
+  evaluate: async formData => {
+    const response = await apiClient.patch('/orbitProfileMonitoring/evaluate', formData);
+    return response.data;
+  },
+};
+
+export default orbitProfileMonitoringService;
