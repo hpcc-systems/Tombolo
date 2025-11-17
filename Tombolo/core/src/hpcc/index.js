f567f1c3cf3821454bb8c90697502663437d6d39
sodon-lnr <sean.odonnell@lexisnexisrisk.com>
2025-11-17 09:50:07 -0500

    Created core package for core backend operations in tombolo. Copied a couple methods from server and created types for them.

---
+++ Tombolo/core/src/hpcc/index.js
@@ -0,0 +1,13 @@
+/**
+ * HPCC-related services and utilities
+ */
+
+const { getClusters } = require('./getClusters');
+const { getClusterOptions } = require('./getClusterOptions');
+const { isClusterReachable } = require('./isClusterReachable');
+
+module.exports = {
+  getClusters,
+  getClusterOptions,
+  isClusterReachable,
+};
