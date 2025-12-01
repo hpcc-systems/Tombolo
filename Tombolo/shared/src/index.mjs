cc39492d987547f27219df2f962d8ddb4c46574e
sodon-lnr <sean.odonnell@lexisnexisrisk.com>
2025-12-01 13:41:52 -0500

    Created duplicate mjs files for shared constants to work on the frontend. Various updates to the frontend and backend for fetching and showing workunit history.

---
+++ Tombolo/shared/src/index.mjs
@@ -0,0 +1,17 @@
+import {
+  UNIT_LOOKUP,
+  relevantMetrics,
+  normalizeLabel,
+  readableLabels,
+} from './workunitConstants.mjs';
+// import parseWorkunitTimestamp from './parseWorkunitTimestamp';
+// import truncateString from './truncateString';
+
+export {
+  UNIT_LOOKUP,
+  relevantMetrics,
+  normalizeLabel,
+  readableLabels,
+  //   parseWorkunitTimestamp,
+  //   truncateString,
+};
