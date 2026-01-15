4ad0189310af8a5227662caacb07fde7289a24d4
sodon-lnr <sean.odonnell@lexisnexisrisk.com>
2026-01-15 10:53:24 -0500

    Deleted old javascript and d.ts files.

--- Tombolo/core/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @tombolo/core
- *
- * Core business logic and shared services
- */
-
-module.exports = require("./src");
--- Tombolo/core/src/database/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Database services and repository functions
- */
-
-// Add database service type definitions here as you create them
-export {};
