8913aac74f2e5fe978b1f408ce08f56cda693737
ydahal2 <yadhap.dahal@lexisnexisrisk.com>
2026-02-19 17:05:41 -0500

    refactor(server): migrate utils/getUserFkIncludes to TypeScript

--- Tombolo/server/utils/getUserFkIncludes.ts
+++ Tombolo/server/utils/getUserFkIncludes.ts
@@ -1,8 +1,14 @@
 import { User } from '../models/index.js';
 
+interface UserInclude {
+  model: typeof User;
+  attributes: string[];
+  as: string;
+}
+
 // Adds creator, updater and approver (if `includeApprover` is true)
-function getUserFkIncludes(includeApprover = false) {
-  const includeUserFks = [
+function getUserFkIncludes(includeApprover = false): UserInclude[] {
+  const includeUserFks: UserInclude[] = [
     {
       model: User,
       attributes: ['firstName', 'lastName', 'email'],
