4e572d949f413c4e463c00826edbac60074abc9f
ydahal2 <yadhap.dahal@lexisnexisrisk.com>
2026-02-20 13:11:07 -0500

    refactor(server): migrate controllers/roleTypesController to TypeScript

--- Tombolo/server/controllers/roleTypesController.ts
+++ Tombolo/server/controllers/roleTypesController.ts
@@ -1,7 +1,8 @@
+import { Request, Response } from 'express';
 import { RoleType } from '../models/index.js';
 import { sendSuccess, sendError } from '../utils/response.js';
 
-const getAllRoles = async (req, res) => {
+const getAllRoles = async (req: Request, res: Response) => {
   try {
     const roles = await RoleType.findAll();
     return sendSuccess(res, roles, 'Roles retrieved successfully');
