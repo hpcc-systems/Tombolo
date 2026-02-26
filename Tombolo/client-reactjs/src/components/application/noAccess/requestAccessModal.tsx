b182d9b32dd07fc3a9fad3c11cc58232a38ed5c2
sodon-lnr <sean.odonnell@lexisnexisrisk.com>
2026-02-26 11:44:54 -0500

    Added back two components.

--- Tombolo/client-reactjs/src/components/application/noAccess/requestAccessModal.tsx
+++ Tombolo/client-reactjs/src/components/application/noAccess/requestAccessModal.tsx
@@ -1,5 +1,52 @@
+// Imports from libraries
 import React from 'react';
+import { Modal, Form, Input, Button } from 'antd';
+import { FormInstance } from 'antd/es/form';
 
-const RequestAccessModal: React.FC<any> = () => null;
+interface RequestAccessModalProps {
+  form: FormInstance;
+  isOpen: boolean;
+  setIsOpen: (open: boolean) => void;
+  onSubmit: () => void;
+}
+
+const RequestAccessModal: React.FC<RequestAccessModalProps> = ({ form, isOpen, setIsOpen, onSubmit }) => {
+  const cancelRequest = () => {
+    form.resetFields();
+    setIsOpen(false);
+  };
+
+  const possiblePlaceholders = [
+    'LET ME IN!!!!',
+    'I NEED ACCESS, LIKE YESTERDAY!!!!',
+    'PLEASE SOMEBODY HELP ME!!!!',
+    'I NEED TO GET IN!!!!',
+    'I NEED ACCESS NOW!!!!',
+  ];
+
+  return (
+    <Modal title="Request Access" open={isOpen} footer={null} onCancel={cancelRequest}>
+      <Form form={form} layout="vertical" onFinish={onSubmit} name="request">
+        <Form.Item
+          label="Additional Information"
+          name="comment"
+          rules={[
+            { required: false, message: 'Please enter your comment' },
+            { max: 256, message: 'Max Additional Information Length is 256 characters' },
+          ]}>
+          <Input.TextArea placeholder={possiblePlaceholders[Math.floor(Math.random() * possiblePlaceholders.length)]} />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+          <Button type="default" onClick={cancelRequest} style={{ marginLeft: '8px' }}>
+            Close
+          </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
 
 export default RequestAccessModal;
